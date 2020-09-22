2b1216b7c5ca2bb0d90c91461497ba048d488268
lawrence ardosa <lawrenceardosa@gmail.com>
2020-09-22 15:34:06 +0800

    Updates

---
+++ precache-manifest.4659b6def61f8abeb8367443da301e51.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d89d07cc2aea8dfce8b557534396b004",
+    "url": "/repo_name/index.html"
+  },
+  {
+    "revision": "c2c8dc21846efa621786",
+    "url": "/repo_name/static/css/2.ba1f8fbc.chunk.css"
+  },
+  {
+    "revision": "29851b5ac90b70e255f8",
+    "url": "/repo_name/static/css/main.dc4f0edc.chunk.css"
+  },
+  {
+    "revision": "c2c8dc21846efa621786",
+    "url": "/repo_name/static/js/2.34c9d522.chunk.js"
+  },
+  {
+    "revision": "b3d198b261321913711d6c108f93f7ca",
+    "url": "/repo_name/static/js/2.34c9d522.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29851b5ac90b70e255f8",
+    "url": "/repo_name/static/js/main.3037f258.chunk.js"
+  },
+  {
+    "revision": "1a8839863479cef6733e",
+    "url": "/repo_name/static/js/runtime-main.ad2cfcca.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/repo_name/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/repo_name/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/repo_name/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/repo_name/static/media/slick.f97e3bbf.svg"
+  }
+]);
